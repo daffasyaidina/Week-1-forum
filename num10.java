bc93e528d9f492cd7eaedb8f53f886115f8bdd41
daffasyaidina <91464359+daffasyaidina@users.noreply.github.com>
2022-03-08 09:47:01 +0700

    Update num10.java

--- num10.java
+++ num10.java
@@ -4,20 +4,22 @@ import java.util.Scanner;
 
 public class num10  {
     public static void main(String[] args) {
+        double first, second, third;
+        double mean, variance, standard_deviation
         
         Scanner num = new Scanner(System.in);
         System.out.println("Enter first num");
-        double first =  num.nextDouble();
+        first =  num.nextDouble();
 
         System.out.println("Enter second num");
-        double second = num.nextDouble();
+        second = num.nextDouble();
 
         System.out.println("Enter third num");
-        double third = num.nextDouble();
+        third = num.nextDouble();
 
-        double mean = (first + second + third) / 3;
-        double variance = (Math.pow((first - mean ), 2) + Math.pow((second - mean), 2) + Math.pow((third - mean), 2)) / 3;
-        double standard_deviation = Math.sqrt(variance);
+        mean = (first + second + third) / 3;
+        variance = (Math.pow((first - mean ), 2) + Math.pow((second - mean), 2) + Math.pow((third - mean), 2)) / 3;
+        standard_deviation = Math.sqrt(variance);
 
         System.out.println("Mean: " + mean);
         System.out.println("variance: " + variance);
